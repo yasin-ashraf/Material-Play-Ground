068783f1b165454d1dac282e757a567601bf0d6b
yasin-ashraf <yasin.asharf@gmail.com>
2020-03-24 16:25:25 +0530

    vector drawables
    preliminary samples

---
+++ app/src/main/java/com/yasin/materialplayground/vectorDrawables/VectorActivity.kt
@@ -0,0 +1,17 @@
+package com.yasin.materialplayground.vectorDrawables
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.yasin.materialplayground.R
+
+/**
+ * Created by Yasin on 24/3/20.
+ */
+
+class VectorActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_vector_drawable)
+  }
+}
