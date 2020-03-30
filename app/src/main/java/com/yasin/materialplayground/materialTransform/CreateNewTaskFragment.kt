8347ef79f7952bba5375c2fb682c1779576eb061
yasin-ashraf <yasin.asharf@gmail.com>
2020-03-30 16:48:21 +0530

    nav setup

--- app/src/main/java/com/yasin/materialplayground/materialTransform/CreateNewTaskFragment.kt
+++ app/src/main/java/com/yasin/materialplayground/materialTransform/CreateNewTaskFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.yasin.materialplayground.R
+import kotlinx.android.synthetic.main.fragment_create_new_task.button_create
 
 /**
  * Created by Yasin on 30/3/20.
@@ -22,5 +24,15 @@ class CreateNewTaskFragment : Fragment() {
     savedInstanceState: Bundle?
   ): View? {
     return inflater.inflate(R.layout.fragment_create_new_task,container,false)
+  }
+
+  override fun onViewCreated(
+    view: View,
+    savedInstanceState: Bundle?
+  ) {
+    super.onViewCreated(view, savedInstanceState)
+    button_create.setOnClickListener {
+      findNavController().navigate(R.id.action_createNewTaskFragment_to_taskCreatedFragment)
+    }
   }
 }
--- app/src/main/java/com/yasin/materialplayground/materialTransform/TaskCreatedFragment.kt
+++ app/src/main/java/com/yasin/materialplayground/materialTransform/TaskCreatedFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.yasin.materialplayground.R
+import kotlinx.android.synthetic.main.fragment_task_created.button_done
 
 /**
  * Created by Yasin on 30/3/20.
@@ -22,5 +24,15 @@ class TaskCreatedFragment : Fragment(){
     savedInstanceState: Bundle?
   ): View? {
     return inflater.inflate(R.layout.fragment_task_created,container,false)
+  }
+
+  override fun onViewCreated(
+    view: View,
+    savedInstanceState: Bundle?
+  ) {
+    super.onViewCreated(view, savedInstanceState)
+    button_done.setOnClickListener {
+      findNavController().navigate(R.id.action_taskCreatedFragment_to_tasksFragment2)
+    }
   }
 }
