833cd5214b1278f98127b2a5d8153fb4d7d4d9be
yasin-ashraf <yasin.asharf@gmail.com>
2020-03-31 20:59:44 +0530

    item touch anim

--- app/src/main/java/com/yasin/materialplayground/materialTransform/TasksAdapter.kt
+++ app/src/main/java/com/yasin/materialplayground/materialTransform/TasksAdapter.kt
@@ -1,12 +1,17 @@
 package com.yasin.materialplayground.materialTransform
 
+import android.transition.TransitionManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.transition.MaterialSharedAxis
 import com.yasin.materialplayground.R
+import com.yasin.materialplayground.viewUtils.FastOutUltraSlowIn
+import kotlinx.android.synthetic.main.list_item_task.view.subTitle
+import kotlinx.android.synthetic.main.list_item_task.view.subTitle_expanded
 
 /**
  * Created by Yasin on 30/3/20.
@@ -17,7 +22,8 @@ class TasksAdapter : ListAdapter<Task, TaskItemViewHolder>(TasksItemDiffCallback
     parent: ViewGroup,
     viewType: Int
   ): TaskItemViewHolder {
-    val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item_task,parent,false)
+    val view = LayoutInflater.from(parent.context)
+        .inflate(R.layout.list_item_task, parent, false)
     return TaskItemViewHolder(view)
   }
 
@@ -25,7 +31,23 @@ class TasksAdapter : ListAdapter<Task, TaskItemViewHolder>(TasksItemDiffCallback
     holder: TaskItemViewHolder,
     position: Int
   ) {
-
+    holder.itemView.setOnClickListener {
+      val fadeThrough = MaterialSharedAxis.create(
+          holder.itemView.context,
+          MaterialSharedAxis.Y, holder.itemView.subTitle.visibility == 8)
+          .apply {
+            duration = 600
+            interpolator = FastOutUltraSlowIn()
+          }
+      TransitionManager.beginDelayedTransition(holder.itemView as ViewGroup, fadeThrough)
+      if (holder.itemView.subTitle.visibility == View.VISIBLE) {
+          holder.itemView.subTitle.visibility = View.GONE
+          holder.itemView.subTitle_expanded.visibility = View.VISIBLE
+      } else {
+          holder.itemView.subTitle_expanded.visibility = View.GONE
+          holder.itemView.subTitle.visibility = View.VISIBLE
+      }
+    }
   }
 
   override fun getItemCount(): Int {
@@ -49,7 +71,7 @@ class TasksItemDiffCallback : DiffUtil.ItemCallback<Task>() {
   }
 }
 
-class TaskItemViewHolder(view: View): RecyclerView.ViewHolder(view)
+class TaskItemViewHolder(view: View) : RecyclerView.ViewHolder(view)
 
 data class Task(
   val id: String,
