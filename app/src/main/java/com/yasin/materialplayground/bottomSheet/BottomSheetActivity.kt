4a42acf45555f37162e9baa7b110d40328947557
yasin-ashraf <yasin.asharf@gmail.com>
2020-04-01 12:31:49 +0530

    google pay arrow anim

--- app/src/main/java/com/yasin/materialplayground/bottomSheet/BottomSheetActivity.kt
+++ app/src/main/java/com/yasin/materialplayground/bottomSheet/BottomSheetActivity.kt
@@ -6,7 +6,11 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.yasin.materialplayground.R
+import com.yasin.materialplayground.normalize
+import kotlinx.android.synthetic.main.activity_bottom_sheet_behaviour.arrow_left
+import kotlinx.android.synthetic.main.activity_bottom_sheet_behaviour.arrow_right
 import kotlinx.android.synthetic.main.activity_bottom_sheet_behaviour.background_container
+import kotlinx.android.synthetic.main.activity_bottom_sheet_behaviour.bg_image
 
 /**
  * Created by Yasin on 31/3/20.
@@ -25,6 +29,9 @@ class BottomSheetActivity : AppCompatActivity() {
         slideOffset: Float
       ) {
         //offset is zero at collapsed state, 1 at expanded state
+        bg_image.alpha = 1 - slideOffset
+        arrow_left.rotation = slideOffset.normalize(0f,1f,-45.0f,0f)
+        arrow_right.rotation = slideOffset.normalize(0f,1f,45.0f,0f)
       }
 
       override fun onStateChanged(
