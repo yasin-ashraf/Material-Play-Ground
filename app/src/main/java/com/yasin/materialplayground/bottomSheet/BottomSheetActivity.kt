0c755728baefc3c5c1ff54092c3f14ca76f274ae
yasin-ashraf <yasin.asharf@gmail.com>
2020-04-01 17:18:17 +0530

    material shape drawable

--- app/src/main/java/com/yasin/materialplayground/bottomSheet/BottomSheetActivity.kt
+++ app/src/main/java/com/yasin/materialplayground/bottomSheet/BottomSheetActivity.kt
@@ -1,21 +1,42 @@
 package com.yasin.materialplayground.bottomSheet
 
+import android.content.res.ColorStateList
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.shape.CornerFamily
+import com.google.android.material.shape.MaterialShapeDrawable
+import com.google.android.material.shape.ShapeAppearanceModel
 import com.yasin.materialplayground.R
 import com.yasin.materialplayground.normalize
 import kotlinx.android.synthetic.main.activity_bottom_sheet_behaviour.arrow_left
 import kotlinx.android.synthetic.main.activity_bottom_sheet_behaviour.arrow_right
 import kotlinx.android.synthetic.main.activity_bottom_sheet_behaviour.background_container
 import kotlinx.android.synthetic.main.activity_bottom_sheet_behaviour.bg_image
+import kotlin.LazyThreadSafetyMode.NONE
 
 /**
  * Created by Yasin on 31/3/20.
  */
 class BottomSheetActivity : AppCompatActivity() {
+
+  private val sheetBgDrawable : MaterialShapeDrawable by lazy(NONE) {
+    val bgContext = background_container.context
+    MaterialShapeDrawable(
+        bgContext,
+        null,
+        R.attr.bottomSheetStyle,
+        0
+    ).apply {
+      fillColor = ColorStateList.valueOf(bgContext.getColor(R.color.terractoca_brick_dark))
+      elevation = resources.getDimension(R.dimen.plane_16) //this is not working
+      shapeAppearanceModel = ShapeAppearanceModel().toBuilder()
+          .setTopLeftCorner(CornerFamily.ROUNDED,resources.getDimension(R.dimen.plane_16))
+          .setTopRightCorner(CornerFamily.ROUNDED,resources.getDimension(R.dimen.plane_16))
+          .build()
+    }
+  }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -32,6 +53,7 @@ class BottomSheetActivity : AppCompatActivity() {
         bg_image.alpha = 1 - slideOffset
         arrow_left.rotation = slideOffset.normalize(0f,1f,-45.0f,0f)
         arrow_right.rotation = slideOffset.normalize(0f,1f,45.0f,0f)
+        sheetBgDrawable.interpolation = 1 - slideOffset
       }
 
       override fun onStateChanged(
@@ -40,7 +62,8 @@ class BottomSheetActivity : AppCompatActivity() {
       ) {
 
       }
-
     })
+
+    background_container.background = sheetBgDrawable
   }
 }
